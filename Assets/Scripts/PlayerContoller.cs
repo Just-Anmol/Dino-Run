1fc5b3d055df59c12745cc3fa55b867cb5fd909e
Anmol Mehto <yashmahto95@gmail.com>
2024-05-09 21:06:31 +0530

    updated

---
+++ Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour
+{
+
+    public float moveSpeed = 5f; // Adjust this value to control the speed of movement
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) )
+        {
+        }
+    }
+}
